4b10c8ce202067708e1f775acd8c5bbafa0f72ad
Torin Blankensmith <torin.blankensmith@gmail.com>
2022-11-07 14:42:59 -0500

    add in option to convert raw GLSL shader park files

--- convertSPCodeToTD.js
+++ convertSPCodeToTD.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
 const converters = require('shader-park-core');
 const src = process.argv[2];
-console.log(JSON.stringify(converters.sculptToTouchDesignerShaderSource(src)));
+const useGLSL = process.argv[3];
+if(useGLSL) {
+    console.log(JSON.stringify(converters.glslToTouchDesignerShaderSource(src)));
+} else {
+    console.log(JSON.stringify(converters.sculptToTouchDesignerShaderSource(src)));
+}
+
